14a48219b22682a4d86f6c92e0498a68d46fd406
Tim Williamson <tim.williamson@kaluza.com>
2022-07-02 14:07:50 +0100

    update run script with new web_video_server path

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 echo "-- source ros --"
 source /opt/ros/galactic/setup.bash
 
+echo "-- source web_video_server --"
+source ../web_video_server/install_aarch64/setup.bash
+
 echo "-- source project --"
 source ./install/setup.bash
 
