d60bc7c7526b96ef04ef984211138d2f65b77d1f
Tim Williamson <tim.williamson@kaluza.com>
2022-07-03 15:17:06 +0100

    swap around sourcing

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 echo "-- source ros --"
 source /opt/ros/galactic/setup.bash
 
-echo "-- source web_video_server --"
-source ../web_video_server/install_aarch64/setup.bash
-
 echo "-- source project --"
 source ./install/setup.bash
+
+echo "-- source web_video_server --"
+source ../web_video_server/install/setup.bash
 
 echo "-- launch! --"
 ros2 launch robot websocket.launch.xml
