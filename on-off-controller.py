8ae9b833ab477bf8fce5ad5f0066f231003502a5
Tim Williamson <timwilliamson1337@gmail.com>
2026-04-09 13:00:23 +0100

    feat: add on-off controller and include on startup in run script

---
+++ on-off-controller.py
@@ -0,0 +1,39 @@
+import gpiozero
+import sys
+from subprocess import check_call
+import getopt
+
+def parse_args(argv):
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:")
+    except getopt.GetoptError:
+        print ('on-off-controller.py -i <input gpio pin> -o <output gpio pin>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('on-off-controller.py -i <input gpio pin> -o <output gpio pin>')
+            sys.exit()
+        elif opt  == "-i":
+            in_pin = arg
+        elif opt == "-o":
+            out_pin = arg
+
+    return in_pin, out_pin
+
+def run(in_pin, out_pin):
+
+    output = gpiozero.OutputDevice(out_pin)
+
+    off_button = gpiozero.Button(in_pin)
+
+    output.on()
+
+    def shutdown():
+        output.off()
+        check_call(['sudo', 'poweroff'])
+
+    off_button.when_pressed = shutdown
+
+if __name__ == "__main__":
+   in_pin, out_pin = parse_args(sys.argv[1:])
+   run(in_pin, out_pin)
--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## Important to start the on-off controller first!
+# This controls the power supply to the pi - when the on button is pressed we get ~20s 
+# to boot up and start outputting a signal that will keep the pi powered up.
+python3 ./on-off-controller.py -i 24 -o 25 &
+
 echo "-- source ros --"
 source /opt/ros/galactic/setup.bash
 
